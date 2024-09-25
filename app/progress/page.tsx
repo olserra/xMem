76865eb9e717e29318d264506b684e02a8297475
Otavio Serra <olserra@gmail.com>
2024-09-25 22:56:18 +0100

    Removing status from skill

--- app/progress/page.tsx
+++ app/progress/page.tsx
@@ -9,7 +9,6 @@ interface Skill {
     id: string;
     name: string;
     progress: number;
-    status: string;
     category: string;
     slug: string;
 }
@@ -47,7 +46,6 @@ const ProgressPage: React.FC = () => {
             id: skillName.toLowerCase(),
             name: skillName,
             progress: 0,
-            status: "To be learned",
             category: skillName.toLowerCase(),
             slug: skillName.toLowerCase().replace(/\s+/g, '-'),
         };
@@ -61,7 +59,6 @@ const ProgressPage: React.FC = () => {
                 body: JSON.stringify({
                     skillId: newSkill.id,
                     currentProgress: newSkill.progress,
-                    status: newSkill.status,
                     user_id: userId,
                 }),
             });
@@ -82,9 +79,8 @@ const ProgressPage: React.FC = () => {
             <h1 className="mt-10 text-2xl">Track Your Skills Progress</h1>
 
             {/* To be learned skills */}
-            <h2 className="text-xl mt-6">To be Learned</h2>
             <div className="skills-container flex flex-col gap-4">
-                {skills.filter(skill => skill.status === "To be learned").map((skill) => (
+                {skills.map((skill) => (
                     <div key={skill.id} className="skill-row flex items-center justify-between border border-1 border-gray-400 rounded-lg p-4">
                         <div className="flex-1">
                             <h2 className="text-lg">{skill.name}</h2>
@@ -92,26 +88,13 @@ const ProgressPage: React.FC = () => {
                                 <div className="bg-blue-500 h-2 rounded-full" style={{ width: '0%' }}></div> {/* 0% progress */}
                             </div>
                         </div>
-                        <p className="text-base text-center w-1/4">To be learned</p>
-                        <Link href={`/${skill.category}/basic`}>
-                            <button className="ml-4 bg-blue-500 text-white rounded-lg px-4 py-2">Start</button>
-                        </Link>
-                    </div>
-                ))}
-            </div>
-
-            {/* In-progress skills */}
-            <h2 className="text-xl mt-6">In Progress</h2>
-            <div className="skills-container flex flex-col gap-4">
-                {skills.filter(skill => skill.status === "In-progress").map((skill) => (
-                    <div key={skill.id} className="skill-row flex items-center justify-between border border-1 border-gray-400 rounded-lg p-4">
-                        <div className="flex-1">
-                            <h2 className="text-lg">{skill.name}</h2>
-                            <div className="progress-bar bg-gray-200 rounded-full h-2">
-                                <div className="bg-blue-500 h-2 rounded-full" style={{ width: `${skill.progress}%` }}></div> {/* Dynamic progress */}
-                            </div>
-                        </div>
-                        <p className="text-base text-center w-1/4">In-progress</p>
+                        {skill.progress === 0 ? (
+                            <p className="text-base text-center w-1/4">To be learned</p>
+                        ) : skill.progress < 100 ? (
+                            <p className="text-base text-center w-1/4">In Progress</p>
+                        ) : (
+                            <p className="text-base text-center w-1/4">Done</p>
+                        )}
                         <Link href={`/${skill.category}/basic`}>
                             <button className="ml-4 bg-blue-500 text-white rounded-lg px-4 py-2">Start</button>
                         </Link>

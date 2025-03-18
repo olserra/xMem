61fcf04cb4273ef12375c85216c77ccb27043567
Otavio Serra <otavio.serra@contractors.roche.com>
2025-03-18 12:12:19 +0000

    Fixing vercel issue type

--- app/types/index.d.ts
+++ app/types/index.d.ts
@@ -4,7 +4,6 @@ export * from './project';
 export * from './userContext';
 export * from './api';
 export * from './forms';
-export * from './metadata';
 export * from './analytics';
 
 // Core Types

6a4004bf8480639dc84590b075197d831eb6babe
Otavio Serra <otavio.serra@contractors.roche.com>
2025-03-18 17:51:16 +0000

    and more fixes

--- app/types/next-auth.d.ts
+++ app/types/next-auth.d.ts
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
+import { Role } from "./auth";
 
 declare module "next-auth" {
     interface Session {
@@ -7,6 +8,7 @@ declare module "next-auth" {
             name: string | null;
             email: string | null;
             image: string | null;
+            role: Role;
         };
     }
 }

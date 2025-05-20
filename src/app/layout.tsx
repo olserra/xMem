d9291b7c2d13412542c93ac08d1d62be885a9de5
Otavio Serra <olserra@gmail.com>
2025-05-20 11:21:40 +0100

    Remove unused import of DashboardShell in layout.tsx to streamline component structure.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import DashboardShell from '../components/layout/DashboardShell'
 
 const inter = Inter({ subsets: ['latin'] })
 

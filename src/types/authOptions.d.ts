7e06d714abdae80269e437e7c40fd9153e300d0f
Otavio Serra <olserra@gmail.com>
2025-06-03 18:50:22 +0100

    Enhance TypeScript type definitions for authOptions module
    - Added a new module declaration for '../auth/[...nextauth]/auth' to improve type support and avoid potential import issues.
    - Ensured consistency in type definitions across different module paths, enhancing maintainability and clarity in the codebase.

--- src/types/authOptions.d.ts
+++ src/types/authOptions.d.ts
@@ -6,4 +6,9 @@ declare module '../app/api/auth/[...nextauth]/auth' {
 declare module 'src/app/api/auth/[...nextauth]/auth' {
   import type { NextAuthOptions } from 'next-auth';
   export const authOptions: NextAuthOptions;
+}
+
+declare module '../auth/[...nextauth]/auth' {
+  import type { NextAuthOptions } from 'next-auth';
+  export const authOptions: NextAuthOptions;
 } 
